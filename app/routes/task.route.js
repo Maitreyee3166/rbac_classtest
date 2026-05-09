492468e37f0315b85a0d329088b9603e8e84eaf9
Maitreyee Samanta <maitreyeesamanta66@ gmail.com>
2026-05-10 02:41:36 +0530

    first commit

---
+++ app/routes/task.route.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const router = express.Router();
+
+const TaskController = require('../controller/task.controller');
+
+
+const UserAuthCheck = require('../middleware/AdminAuthCheck')
+const RoleAuthCheck = require('../middleware/roleAuthCheck');
+
+
+const TaskImage = require('../utils/cloudinary');
+
+
+
+router.get('/', UserAuthCheck, RoleAuthCheck('admin', 'manager'), TaskController.alltask);
+router.get('/employeeid/:id', UserAuthCheck, RoleAuthCheck('employee'), TaskController.ownTask); 
+router.get('/:id', UserAuthCheck, RoleAuthCheck('employee','manager'), TaskController.taskById);
+
+router.post('/', UserAuthCheck, RoleAuthCheck('manager'), TaskImage.single('attachments'), TaskController.taskCreate);
+
+router.put('/:id', UserAuthCheck, RoleAuthCheck('admin', 'employee'), TaskImage.single('attachments'), TaskController.taskUpdate);
+
+router.delete('/:id', UserAuthCheck, RoleAuthCheck('admin'), TaskController.deleteTask);
+
+router.patch('/status/:id', UserAuthCheck, RoleAuthCheck('manager', 'employee'), TaskController.updateTaskStatus);
+router.patch('/assign/:id', UserAuthCheck, RoleAuthCheck('manager'), TaskController.updateTaskAssign);
+
+router.get('/search', TaskController.searchTask);
+
+
+
+
+module.exports = router;
